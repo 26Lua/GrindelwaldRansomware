502c1d0f8726752644d7e813c6d9df75819d4e7c
Lua <69074210+26Lua@users.noreply.github.com>
2022-06-01 20:18:09 +0800

    Update grindeldecrypt.py

--- grindeldecrypt.py
+++ grindeldecrypt.py
@@ -1,3 +1,18 @@
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+#            _           _      _               _     _ #
+#           (_)         | |    | |             | |   | |#
+#  __ _ _ __ _ _ __   __| | ___| |_      ____ _| | __| |#
+# / _` | '__| | '_ \ / _` |/ _ \ \ \ /\ / / _` | |/ _` |#
+#| (_| | |  | | | | | (_| |  __/ |\ V  V / (_| | | (_| |#
+# \__, |_|  |_|_| |_|\__,_|\___|_| \_/\_/ \__,_|_|\__,_|#
+#  __/ |                                                #
+# |___/                                                 #                                                    
+#           By Lua, (Educational Purposes Only)         #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+
+#DISCLAIMER: THIS IS ACTIVE MALWARE, IF YOU RUN IT AND FORGET THE PASSWORD THEN CHECK THE SOURCE CODE (line:71)
+#I HAVE WRITTEN BOTH .PY SCRIPTS IN ONE FILE SO IT CANNOT BE RUN BY ACCIDENT. (Indentation for python is also a bit iffy for these files)
+
 #DECRYPTION CODE (grindeldecrypt.py)
 
 import os
